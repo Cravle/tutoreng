e100a728bb468da904ea3411ac572e990d7fbb71
Vlad <vladoxer280502@gmail.com>
2023-04-02 18:57:23 +0300

    test ned folder structure

--- apps/ui/src/stores/user.store.ts
+++ apps/ui/src/stores/user.store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { User } from '@tutoreng/d    b'
+import type { User } from '@tutoreng/db'
 
 interface IUserStore {
   user: Omit<User, 'password'> | null
--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [react(), svgr()],
 
   server: {
     port: 8000,
-  },
-  optimizeDeps: {
-    include: ['@tutoreng/db', '@tutoreng/shared'],
   },
 })
